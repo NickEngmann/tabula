4c5295e77c9710b39f4478f112b7f9a416c0d847
NickySlicksHaha <nickengmann_2@yahoo.com>
2016-12-20 00:26:14 -0500

    added authentification

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    //Live Connect API information
+    clientId: '14aa0ab2-0317-4b92-894d-3c147fb74766',
+    clientSecret: 'dyHWi46eGZwkeZwYF8ChBGu',
+    redirectUrl: 'http://localhost:3000/callback'
+};
