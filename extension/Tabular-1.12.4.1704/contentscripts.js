1b2e36a49999562536f6e323fb758ee397f3cbcd
NickySlicksHaha <nickengmann_2@yahoo.com>
2017-01-05 21:10:59 -0500

    replacing all advertisements with empty divs

---
+++ extension/Tabular-1.12.4.1704/contentscripts.js
@@ -0,0 +1,12 @@
+'use strict';
+
+window.addEventListener("load", function() {
+	var html = document.querySelector('body');
+	html.setAttribute('tabul', '');
+	html.setAttribute('tabsd', '');
+
+	var tabularDiv = document.createElement('div');
+	tabularDiv.setAttribute('id', 'tabularDiv');
+	html.appendChild(tabularDiv);
+
+});
